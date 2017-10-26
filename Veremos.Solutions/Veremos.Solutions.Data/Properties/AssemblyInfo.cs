c1a01bad316cca6203b75fd9a3d48fb593388d52
Raphael Saldanha <raphaelsaldanha@gmail.com>
2017-10-26 16:07:15 +0200

    Project setup

---
+++ Veremos.Solutions/Veremos.Solutions.Data/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Veremos.Solutions.Data")]
+[assembly: Guid("f2d07923-d029-4f78-9f17-6d8d9240d645")]
+[assembly: ComVisible(false)]
